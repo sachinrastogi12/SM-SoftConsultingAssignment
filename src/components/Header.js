33043a0e2de929bf81d8a2112531d4535e556c66
sachinrastogipvt <sachinrastogipvt@gmail.com>
2022-11-05 20:49:02 +0530

    SM Soft Solution

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = ({ handleToggleDarkMode }) => {
+	return (
+		<div className='header'>
+			<h1>BookMark Manager</h1>
+			
+		</div>
+	);
+};
+
+export default Header;
